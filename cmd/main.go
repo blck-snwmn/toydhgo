9dff6ef256b8ea43e04d08a1f5ad60adc902e50e
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-01 01:57:54 +0900

    Refactor Diffie-Hellman key exchange implementation

--- cmd/main.go
+++ cmd/main.go
@@ -7,54 +7,53 @@ import (
 )
 
 func main() {
-	// var (
-	// 	p *big.Int // big prime
-	// 	q *big.Int // big prime
-	// )
-	// p := new(big.Int)
-	// p.SetString("58258341155718285019", 10)
+	p, _, g := gen()
 
-	// q := new(big.Int)
-	// q.SetString("59867257146064236763", 10)
-	// fmt.Println(p)
-	// fmt.Println(q)
+	alice := newDH(p, g)
+	bob := newDH(p, g)
+	alicesKey := alice.getSharedKey(bob.publicKey)
+	bobsKey := bob.getSharedKey(alice.publicKey)
 
-	p, _ := rand.Prime(rand.Reader, 100)
-	q, _ := rand.Prime(rand.Reader, 50)
-	fmt.Printf("p: %v\n", p)
-	fmt.Printf("q: %v\n", q)
+	fmt.Printf("alice's key: %v\n", alicesKey)
+	fmt.Printf("bob's key: %v\n", bobsKey)
 
-	pp := new(big.Int).Sub(p, big.NewInt(1)) // p-1
-	fmt.Printf("pp: %v\n", pp)
-
-	h, _ := rand.Int(rand.Reader, pp) // random number in [0, p-1). TODO (1, p-1)
-	fmt.Printf("h: %v\n", h)
-
-	ppq := new(big.Int).Div(pp, q) // (p-1)/q
-	fmt.Printf("ppq: %v\n", ppq)
-
-	g := new(big.Int).Exp(h, ppq, p)
-
-	fmt.Printf("g: %v\n", g)
-
-	xa, _ := rand.Int(rand.Reader, big.NewInt(10))
-	xb, _ := rand.Int(rand.Reader, big.NewInt(10))
-	fmt.Printf("a: %v\n", xa)
-	fmt.Printf("b: %v\n", xb)
-
-	ya := new(big.Int).Exp(g, xa, p)
-	yb := new(big.Int).Exp(g, xb, p)
-	fmt.Printf("ya: %v\n", ya)
-	fmt.Printf("yb: %v\n", yb)
-
-	zz1 := new(big.Int).Exp(yb, xa, p)
-	zz2 := new(big.Int).Exp(ya, xb, p)
-	fmt.Printf("zz1: %v\n", zz1)
-	fmt.Printf("zz2: %v\n", zz2)
-
-	if zz1.Cmp(zz2) == 0 {
+	if alicesKey.Cmp(bobsKey) == 0 {
 		fmt.Println("OK")
 	} else {
 		fmt.Println("NG")
 	}
+}
+
+func gen() (p *big.Int, q *big.Int, g *big.Int) {
+	p, _ = rand.Prime(rand.Reader, 100)
+	q, _ = rand.Prime(rand.Reader, 50)
+
+	pp := new(big.Int).Sub(p, big.NewInt(1)) // p-1
+
+	h, _ := rand.Int(rand.Reader, pp) // random number in [0, p-1). TODO (1, p-1)
+
+	ppq := new(big.Int).Div(pp, q) // (p-1)/q
+
+	g = new(big.Int).Exp(h, ppq, p)
+	return
+}
+
+func newDH(p, g *big.Int) *dh {
+	x, _ := rand.Int(rand.Reader, big.NewInt(10))
+	y := new(big.Int).Exp(g, x, p)
+	return &dh{
+		prime:      p,
+		privateKey: x,
+		publicKey:  y,
+	}
+}
+
+type dh struct {
+	prime      *big.Int
+	privateKey *big.Int
+	publicKey  *big.Int
+}
+
+func (d *dh) getSharedKey(otherPubicKey *big.Int) *big.Int {
+	return new(big.Int).Exp(otherPubicKey, d.privateKey, d.prime)
 }
